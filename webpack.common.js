f092c267a713b0c2a95059768fc7a07354d06f08
Christoph Haider <christophhaider@Christophs-MacBook-Pro-2019.local>
2023-09-02 23:42:23 +0200

    split webpack config

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+module.exports = {
+  entry: './src/main.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new CopyWebpackPlugin({ patterns: [{ from: 'src/assets', to: 'assets' }] }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'favicon', to: 'favicon' }],
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+}
